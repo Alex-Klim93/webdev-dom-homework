b7b5178127e473c2ea5e42249f2be8a0039654fc
Alex-Klim93 <melkimelki79@gmail.com>
2025-07-02 12:53:25 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 import { renderComments } from "./modules/renderComments.js";
 import { addComment } from "./modules/addComment.js";
-import { sanitizeInput } from "./modules/sanitizeInput.js";
 import { updateTasks } from "./modules/comments.js";
 
-fetch("https://wedev-api.sky.pro/api/v1/Alex-Klim93/comments").then(
-  (response) => {
-    return response.json();
-  }).then((data)=>{
-    console.log(data);
-    updateTasks(data.comments)
-    renderComments()
-  })
 
-//   fetch("https://wedev-api.sky.pro/api/v1/Alex-Klim93/comments",{
-//     method:'POST',
-//     body: JSON.stringify(newComment),
-//   }).then(response => {
-//     return response.json();
-// }).then(data =>{
-//     console.log(data);
-//     updateTasks(data.comments);
-//     renderComments();
-// })
+const loadingMessage = {
+  author: { name: "" },
+  text: "Пожалуйста подождите, идет загрузка...",
+  date: "",
+  likes: 0,
+  isLiked: false,
+  isPlainText: true, // Флаг для стилизации (если нужно)
+};
+
+
+updateTasks([loadingMessage]);
+renderComments();
+
+
+fetch("https://wedev-api.sky.pro/api/v1/Alex-Klim93/comments")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Сервер не отвечает");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    updateTasks(data.comments); // Обновляем список
+    renderComments(); // Перерисовываем
+    addComment(); // Активируем кнопку отправки
+  })
+  .catch((error) => {
+    console.error("Ошибка:", error);
+    // Показываем сообщение об ошибке
+    updateTasks([{
+      author: { name: "" },
+      text: "Не удалось загрузить комментарии. Попробуйте позже.",
+      isPlainText: true,
+    }]);
+    renderComments();
+  });
